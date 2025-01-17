06ef2b6b9ace1e2aeaada72888e3b2c289ad9916
Vinícius Mello <vinicius.moreira.mello@gmail.com>
2025-01-17 13:04:39 +0000

    get set

--- orfc.h
+++ orfc.h
@@ -10,6 +10,30 @@
 #include "ht.h"
 #include "file.h"
 
+#define get(a, k) _Generic((k), int: array_get, \
+                              obj: ht_get)(a, k)
+#define set(a, k, v) _Generic((k), int: array_set, \
+                              obj: ht_set)(a, k, v)
+#define num(v) _Generic((v), int: num_int, double: num_double)(v)
+
+inline int len(obj a) {
+  if(is_str(a)) return str_len(a);
+  if(is_array(a)) return array_len(a);
+  return 0;
+} 
+
+inline void print(obj a) {
+  obj_print(a);
+}
+
+inline void println(obj a) {
+  obj_println(a);
+}
+
+inline obj str(obj a) {
+  return str_obj(a);
+}
+
 void orfc_init();
 
 #endif
